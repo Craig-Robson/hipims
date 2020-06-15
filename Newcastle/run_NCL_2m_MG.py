d2138bf9d9dd3dff9d01b482f01851d20d6b8d5f
Xiaodong Ming <xiaodong.ming@outlook.com>
2020-06-15 22:37:20 +0100

    Update run_NCL_2m_MG.py

--- Newcastle/run_NCL_2m_MG.py
+++ Newcastle/run_NCL_2m_MG.py
@@ -8,7 +8,7 @@ from hipims_io.Raster import Raster
 file_path = os.path.dirname(os.path.abspath(__file__))
 # the absolute path of the model executable file
 model_name = os.path.dirname(file_path)+'/release/bin/hipims-flood-mgpus'
-def run_mg(case_folder, model_name):
+def run_mg(model_name):
     start = time.perf_counter()
     input_obj = hp.load_object('obj_in')
     input_obj.Summary.display()
@@ -21,4 +21,4 @@ def run_mg(case_folder, model_name):
 
 
 if __name__ == '__main__':
-    run_mg(case_folder, model_name)
+    run_mg(model_name)
