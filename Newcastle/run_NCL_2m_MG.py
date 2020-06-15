c99087814ede789d07bfcc5a9904ee63278f4985
Xiaodong Ming <xiaodong.ming@outlook.com>
2020-06-15 14:19:58 +0100

    change paths of data and model

--- Newcastle/run_NCL_2m_MG.py
+++ Newcastle/run_NCL_2m_MG.py
@@ -6,14 +6,14 @@ import pandas as pd
 from hipims_io import InputHipims
 #from hipims_io import OutputHipims
 from hipims_io.Raster import Raster
-
-def run_mg():
+file_path = os.path.dirname(os.path.abspath(__file__))
+data_folder = file_path # data folder is the same with this script
+# case_folder is the path to store input and output data of Hipims
+case_folder = file_path+'/Model' 
+# the absolute path of the model executable file
+model_name = os.path.dirname(file_path)+'/release/bin/hipims-flood-mgpus'
+def run_mg(case_folder, model_name, num_gpus=8):
     start = time.perf_counter()
-    # Create input files
-    # assume the current dir is the dir to run the model
-    case_folder = os.getcwd() + '/MG'
-    num_gpus = 8
-    data_folder = '../Data'
     dem_file = data_folder + '/DEM2m.gz'
     rain_mask_obj = Raster(data_folder + '/rain_mask_UO_radar.asc')
     rain_source_mat = np.loadtxt(data_folder + '/rain_source_data_1.csv',
@@ -29,14 +29,10 @@ def run_mg():
     #input_obj.set_device_no([0,1,2,3]) #Tesla K80
     input_obj.set_rainfall(rain_mask=rain_mask_obj.array, rain_source=rain_source)
     input_obj.set_gauges_position(gauges_pos=gauges_pos)
-    # case_obj.Summary.add_items('Reference datetime', '2019-06-10 03:00')
     input_obj.write_input_files()
-    input_obj.Summary.write_readme()
-    #input_obj.save_object(case_folder + '/input_obj_MG.pickle')
     input_obj.Summary.display()
     # run model
     os.chdir(case_folder)
-    model_name = '../../hipims/release/bin/hipims-flood-mgpus'
     os.system(model_name)
     end = time.perf_counter()
     print('Model runtime (s):')
@@ -44,4 +40,4 @@ def run_mg():
 
 
 if __name__ == '__main__':
-    run_mg()
+    run_mg(case_folder, model_name)
