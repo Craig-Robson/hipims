32d305e895490c1edf02a23dc05177588e767ca5
Xiaodong Ming <xiaodong.ming@outlook.com>
2020-06-16 15:55:21 +0100

    Update combine_mgpu_results.py

--- Newcastle/combine_mgpu_results.py
+++ Newcastle/combine_mgpu_results.py
@@ -14,14 +14,16 @@ if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 obj_out = hp.load_object('obj_out')
 os.chdir(output_folder)
-# save gauge data
-gauges_pos, times, values = obj_out.read_gauges_file('h')
-np.savetxt('gauges_pos.csv', gauges_pos, fmt='%g', delimiter=',')
-np.savetxt('gauges_depth.csv', values, fmt='%g', delimiter=',')
-np.savetxt('time_steps.csv', times, fmt='%g', delimiter=',')
-# save grid data
-grid_file_tags = obj_out.grid_file_tags
-for file_tag in grid_file_tags:
-    obj_h = obj_out.read_grid_file(file_tag)
-    obj_h.to_osgeo_raster(file_tag)
+
+def combine_save(obj_out=obj_out):
+    # save gauge data
+    gauges_pos, times, values = obj_out.read_gauges_file('h')
+    np.savetxt('gauges_pos.csv', gauges_pos, fmt='%g', delimiter=',')
+    np.savetxt('gauges_depth.csv', values, fmt='%g', delimiter=',')
+    np.savetxt('time_steps.csv', times, fmt='%g', delimiter=',')
+    # save grid data
+    grid_file_tags = obj_out.grid_file_tags
+    for file_tag in grid_file_tags:
+        obj_h = obj_out.read_grid_file(file_tag)
+        obj_h.to_osgeo_raster(file_tag)
     
