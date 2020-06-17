85681cb46e37ed2d04704b6a03b3f616338ed5b4
Xiaodong Ming <xiaodong.ming@outlook.com>
2020-06-17 11:10:50 +0100

    rectify the path of obj_in and obj_out

--- Newcastle/run_NCL_2m_MG.py
+++ Newcastle/run_NCL_2m_MG.py
@@ -8,6 +8,7 @@ import pandas as pd
 import hipims_io as hp
 from hipims_io.Raster import Raster
 file_path = os.path.dirname(os.path.abspath(__file__))
+case_path = os.path.dirname(file_path)
 # the absolute path of the model executable file
 model_name = os.path.dirname(file_path)+'/release/bin/hipims-flood-mgpus'
 def run_mg(model_name=model_name, rain_source_file=None, run_time=None):
@@ -16,7 +17,7 @@ def run_mg(model_name=model_name, rain_source_file=None, run_time=None):
     run_time: [0, 10800, 600, 108000]
     """
     start = time.perf_counter()
-    input_obj = hp.load_object('../obj_in')
+    input_obj = hp.load_object(case_path+'/obj_in')
     if rain_source_file is not None:
         rain_source_mat = np.loadtxt(rain_source_file, 
                                      delimiter=',')
@@ -31,7 +32,7 @@ def run_mg(model_name=model_name, rain_source_file=None, run_time=None):
     output_file_tags = ['h_'+str(t) for t in np.arange(run_time[0], run_time[1]+run_time[2], run_time[2])]
     output_file_tags.append('h_max_'+str(run_time[1]))
     obj_out.grid_file_tags = output_file_tags
-    obj_out.save_object('obj_out')
+    obj_out.save_object(case_path+'obj_out')
     input_obj.Summary.display()
     time.sleep(5)
     # run model
