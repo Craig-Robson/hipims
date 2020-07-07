5ffbf4695547fba2377a299616c25f47573e7dee
Ringo Sham <ringosham@gmail.com>
2020-07-07 22:11:41 +0100

    Fixed finding output files for sending

--- Newcastle/KafkaProducer.py
+++ Newcastle/KafkaProducer.py
@@ -1,12 +1,14 @@
 import glob
 import os
 from kafka import KafkaProducer
+from kafka.errors import KafkaError
 
-output_path = "/hipims/Outputs"
-output_files = glob.glob(output_path + "/*.gz")
+
 
 def send_files():
     print("Connecting to Kafka as producer")
+    output_path = "/hipims/Outputs"
+    output_files = glob.glob(output_path + "/*.gz")
     print(output_files)
     # Set maxiumum request size to 25000000 bytes (25MB)
     producer = KafkaProducer(bootstrap_servers="10.79.253.132:30002", max_request_size=25000000)
@@ -15,5 +17,10 @@ def send_files():
         print("sending " + filename)
         file_handle = open(file, 'rb')
         # This might be slow since we're reading the entire file to bytes
-        producer.send(topic="hipims", value=file_handle.read(), key=filename)
+        response = producer.send(topic="hipims", value=file_handle.read(), key=filename)
+        try:
+            response.get(timeout=10)
+        except KafkaError as e:
+            print(e)
+            pass
         file_handle.close()
