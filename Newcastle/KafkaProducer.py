da4247806d9585086fb8437e95b00b29296af33a
Ringo Sham <ringosham@gmail.com>
2020-07-08 01:03:04 +0100

    Producer debug info

--- Newcastle/KafkaProducer.py
+++ Newcastle/KafkaProducer.py
@@ -16,8 +16,10 @@ def send_files():
         filename = file[file.rfind("/") + 1:len(file)]
         print("sending " + filename)
         file_handle = open(file, 'rb')
+        file_data = file_handle.read()
+        print("file size: " + len(file_data) + " bytes")
         # This might be slow since we're reading the entire file to bytes
-        response = producer.send(topic="hipims", value=bytearray(file_handle.read()), key=filename)
+        response = producer.send(topic="hipims", value=file_data, key=filename)
         try:
             response.get(timeout=10)
         except KafkaError as e:
