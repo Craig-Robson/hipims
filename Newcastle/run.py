0bc2e1901884aa9559a0c02c817ab32ac71f9612
Craig Robson <craig.robson1@ncl.ac.uk>
2021-06-03 14:30:33 +0100

    set run script based on kafka script to run hipims but without kafka

---
+++ Newcastle/run.py
@@ -0,0 +1,48 @@
+import gzip
+import json
+import os
+import time
+
+import combine_mgpu_results
+import run_NCL_2m_MG
+import tempfile
+import zipfile
+import shutil
+from datetime import datetime
+
+print("Running")
+print("Unzipping forecasts")
+
+data = 'data/dataset_1.csv'
+tmp = data
+
+# Unzipped forecasts located in /tmp/{random directory} (Use the tmp variable)
+print(f"Starting simulation at {datetime.now()}")
+for forecast_file in os.listdir(tmp):
+    print(f"Running simulator for {forecast_file}...")
+    run_NCL_2m_MG.run_mg(rain_source_file=os.path.join(tmp, forecast_file), run_time=[0, 10800, 600, 108000])
+    print(f"Combining results...")
+    try:
+        combine_mgpu_results.combine_save()
+    except Exception as e:
+        print(e)
+        # Ignore any exceptions for now.
+        pass
+    print(f"Sending output to Kafka")
+
+    print(f"Preparing next simulation...")
+    output_path = "/hipims/Outputs"
+    for hipims_output in os.listdir(output_path):
+        print('copy files to output dir')
+
+# with open('rain_source_data_1.csv', mode='wb+') as rain_source:
+#     rain_source.write(data)
+#
+# time.sleep(5)
+# print('data set up...')
+# rain_source_file = os.getcwd()+'/rain_source_data_1.csv'
+# run_NCL_2m_MG.run_mg(rain_source_file=rain_source_file, run_time=[0, 10800, 600, 108000])
+print(f"Simulation ended at {datetime.now()}")
+print("Cleaning up input files")
+os.remove(tmp_zip.name)
+shutil.rmtree(tmp)
